3c56a60afdc26aa0c19aada89e544c746260c5af
chhavisujeebun <chhavisujeebun@cmail.carleton.ca>
2022-02-26 22:09:43 -0500

    Implemented proximity sensor to avoid obstacles in training map 1 using python

---
+++ obstacle_avoidance_proximitysensor.py
@@ -0,0 +1,71 @@
+'''
+SYSC 4805 
+Group L2-3
+'''
+
+#Try to connect to coppeliasim python API library
+try:
+    import sim
+except:
+    print ('--------------------------------------------------------------')
+    print ('"sim.py" could not be imported. This means very probably that')
+    print ('either "sim.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "sim.py"')
+    print ('--------------------------------------------------------------')
+    print ('')
+
+import time as t
+import math
+
+global clientID
+
+def setWheelVelocity(handle, velocity):
+    return sim.simxSetJointTargetVelocity(clientID, handle, velocity, sim.simx_opmode_oneshot)
+
+def getObjectHandle(obj_name):
+    return sim.simxGetObjectHandle(clientID, obj_name, sim.simx_opmode_blocking)
+
+def startSimulation():
+    print ('Program started')
+    sim.simxFinish(-1)
+    return sim.simxStart('127.0.0.1',19999,True,True,5000,5)
+
+
+    
+
+if __name__ == "__main__":
+
+    clientID = startSimulation()
+    
+    if clientID!=-1:
+        print ('Connected to remote API server')
+        sim.simxAddStatusbarMessage(clientID,'Python Script Connected.',sim.simx_opmode_oneshot)
+
+        #Get wheel and proximity sensor ObjectHandles
+        #res,model = getObjectHandle('robot')
+        res, left_joint  = getObjectHandle('left_joint1')
+        res, right_joint = getObjectHandle('right_joint1')
+        res, prox_sensor = getObjectHandle('Proximity_sensor0')
+        #res, line_sensor = getObjectHandle('Line_Sensor')
+
+        while True:
+
+            res = sim.simxGetObjectGroupData(clientID, 5, 13, sim.simx_opmode_blocking)
+            returnCode,detectionState = sim.simxReadProximitySensor(clientID, prox_sensor, sim.simx_opmode_streaming)[0:2] #simx_opmode_streaming
+            print(detectionState)
+            if detectionState:
+            #Make Bot Turn #turning left
+                print("turning")
+                res = setWheelVelocity(left_joint, 100*math.pi/180)
+                res = setWheelVelocity(right_joint, 50*math.pi/180)
+                t.sleep(2)
+            else:
+            #Make Bot Move straight
+                res = setWheelVelocity(left_joint, 200*math.pi/-180)
+                res = setWheelVelocity(right_joint, 200*math.pi/-180)    
+        sim.simxGetPingTime(clientID)
+        sim.simxFinish(clientID)
+    else:
+        print ('Failed connecting to remote API server')
+    print ('Program ended')
